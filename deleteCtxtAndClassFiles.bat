c98b097de853a07e2f090f44e3edb658efb1ac0b
MrsFreckles <luna.kreyenberg@gmail.com>
2024-08-02 01:14:44 +0200

    init

---
+++ deleteCtxtAndClassFiles.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM Navigiere zum src Verzeichnis
+cd ...
+
+REM Lösche alle .class Dateien
+del /S /Q *.class
+
+REM Lösche alle .ctxt Dateien
+del /S /Q *.ctxt
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import greenfoot.World;
+
+public class Main extends World {
+    public Main() {
+        super(600, 400, 1);
+    }
+}
